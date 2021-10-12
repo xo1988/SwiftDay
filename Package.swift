fdf7559143fda5d56fdb1629dd0cb292b7ad52be
wangyu <wangyu@wisetv.com.cn>
2021-10-12 14:15:59 +0800

    包管理编写错误

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@
 import PackageDescription
 
 let package = Package(name: "SwiftDay",
-                      platforms: [.macOS(.v10_12),
-                                  .iOS(.v10),
-                                  .tvOS(.v10),
-                                  .watchOS(.v3)],
-                      products: [.library(name: "SwiftDay",
-                                          targets: ["SwiftDay"])],
-                      targets: [.target(name: "SwiftDay",
-                                        path: "Source")]),
-                                .testTarget(name: "SwiftDayTests",
-                                            dependencies: ["SwiftDay"],
-                                            path: "SwiftDayTests")],
-                      swiftLanguageVersions: [.v5])
+                  platforms: [.macOS(.v10_12),
+                              .iOS(.v10),
+                              .tvOS(.v10),
+                              .watchOS(.v3)],
+                  products: [.library(name: "SwiftDay",
+                                      targets: ["SwiftDay"])],
+                  targets: [.target(name: "SwiftDay",
+                                    path: "Source"),
+                            .testTarget(name: "SwiftDayTests",
+                                        dependencies: ["SwiftDay"],
+                                        path: "SwiftDayTests")],
+                  swiftLanguageVersions: [.v5])
